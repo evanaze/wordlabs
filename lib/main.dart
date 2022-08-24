2bec6de7864304d911c51ab74b1b32d03eb0a1ac
Evan Azevedo <evan@azevedo.com>
2022-08-23 19:38:26 -0700

    working on app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:mongo_dart/mongo_dart.dart' show Db, DbCollection;
+
+
+class DBConnection {
+
+  static DBConnection _instance;
+
+  final String _host = "DATABASE SERVER";
+  final String _port = "DATABASE PORT";
+  final String _dbName = "DATABASE NAME";
+  Db _db;
+
+  static getInstance(){
+    if(_instance == null) {
+      _instance = DBConnection();
+    }
+    return _instance;
+  }
+
+  Future<Db> getConnection() async{
+    if (_db == null){
+      try {
+        _db = Db(_getConnectionString());
+        await _db.open();
+      } catch(e){
+        print(e);
+      }
+    }
+    return _db;
+  }
+
+  _getConnectionString(){
+    return "mongodb://$_host:$_port/$_dbName";
+  }
+
+  closeConnection() {
+    _db.close();
+  }
+}
 
 
 String readFileAsync() {
-  String contents = new File('./assets/test.html').readAsStringSync();
+  String contents = new File('assets/test.html').readAsStringSync();
   return contents;
 }
 
