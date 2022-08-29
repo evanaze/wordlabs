8ac5a7767cb9a586fcfa3bdb381c3aa08bfc054a
Evan Azevedo <evan@azevedo.com>
2022-08-29 07:48:44 -0700

    got the app working

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
 
 
 Future<String> makeRequest() async {
-    var url = Uri(
-      scheme: 'http',
-      host: 'localhost',
-      port: 8080);
+    var url = Uri.parse('http://localhost:8080');
     var response = await http.get(url);
     //If the http request is successful the statusCode will be 200
     if(response.statusCode == 200){
@@ -22,19 +19,16 @@ Future<String> makeRequest() async {
 void main() => runApp(const MyApp());
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'WordLabs', home: HomePage());
-  }
 }
 
 class _MyAppState extends State<MyApp> {
+  late Future<String> _content;
+
   @override
   void initState() {
     super.initState();
@@ -49,69 +43,59 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      body: SafeArea(
-        child: FutureBuilder<String>(
-          future: _content,
-          builder: (context, snapshot) {
-            List<Widget> children;
-            if (snapshot.hasData) {
-              children = <SingleChildScrollView>[
-                const Html(
-                  data: _content,
-                  tagsList: Html.tags,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WordLabs'),
+        ),
+        body: SafeArea(
+          child: FutureBuilder<String>(
+            future: _content,
+            builder: (context, snapshot) {
+              List<Widget> children;
+              if (snapshot.hasData) {
+                return Html(
+                  data: snapshot.data,
                   style: {
-                    "table": Style(
-                      backgroundColor: Color.fromARGB(0x50, 0xee, 0xee, 0xee),
-                    ),
-                    "tr": Style(
-                      border: Border(bottom: BorderSide(color: Colors.grey)),
-                    ),
-                    "th": Style(
-                      padding: EdgeInsets.all(6),
-                      backgroundColor: Colors.grey,
-                    ),
-                    "td": Style(
-                      padding: EdgeInsets.all(6),
-                      alignment: Alignment.topLeft,
-                    ),
-                    'h5': Style(maxLines: 2, textOverflow: TextOverflow.ellipsis),
+                    'h1': Style(color: Colors.red),
+                    'p': Style(color: Colors.black87, fontSize: FontSize.medium),
+                    'ul': Style(margin: const EdgeInsets.symmetric(vertical: 20))
                   },
+                );
+              } else if (snapshot.hasError) {
+                children = <Widget>[
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                ];
+              } else {
+                children = const <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  )
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
                 ),
-              ];
-            } else if (snapshot.hasError) {
-              children = <Widget>[
-                const Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text('Error: ${snapshot.error}'),
-                )
-              ];
-            } else {
-              children = const <Widget>[
-                SizedBox(
-                  width: 60,
-                  height: 60,
-                  child: CircularProgressIndicator(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16),
-                  child: Text('Awaiting result...'),
-                )
-              ];
+              );
             }
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: children,
-              ),
-            );
-          }
-        )
-      ),
+          )
+        ),
+      )
     );
   }
 }
